939c5bcec0eed9e1deb5aa7ec194f29066a359e9
Mikael Tiba <mikaeltiba@Mikaels-Air.arecibo.inter.edu>
2024-05-15 11:40:23 -0400

    Update

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # proyectoFinal3015
-# Proyecto final COMP3015
 # Mikael Hashem K. Tiba
 # R00627615
---
+++ app/controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once 'UserController.php';
+
+class HomeController {
+    public function index() {
+
+        $userController = new UserController();
+        $data = $userController->listUsers(); // Asume que listUsers() es un método en UserController
+
+        // require_once "app/views/home.php";
+        include 'app/views/home.php';
+
+        // $userController = new UserController();
+        // $userController->listUsers(); // Esto cargará la vista de lista de usuarios
+    }
+}
+
+?>
